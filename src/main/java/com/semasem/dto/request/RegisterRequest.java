884966eaa1cebb8aa5cdad7bab118214916d6296
Данил Петров <petrov@rentminder.ru>
2025-10-25 16:12:02 +0300

    change

--- src/main/java/com/semasem/dto/request/RegisterRequest.java
+++ src/main/java/com/semasem/dto/request/RegisterRequest.java
@@ -12,8 +12,6 @@ public class RegisterRequest {
     private String name;
     @Email
     private String email;
-    @Pattern(regexp = "^\\+7\\d{10}$", message = "Phone must be in format +7XXXXXXXXXX")
-    private String phoneNumber;
     @Size(min = 8, max = 255, message = "The password size should be from 8 to 10")
     private String password;
 }
